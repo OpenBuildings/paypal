0b8998f6358a2b39ad1041c35a93c8419a063a47
Haralan Dobrev <hkdobrev@gmail.com>
2013-10-30 14:57:48 +0200

    Make Payment::verify_ipn() static and remove obsolete $data argument

--- src/OpenBuildings/PayPal/Payment.php
+++ src/OpenBuildings/PayPal/Payment.php
@@ -124,6 +124,86 @@ abstract class Payment {
 		return Payment::$environment;
 	}
 
+	/**
+	 * Validates an IPN request from Paypal.
+	 */
+	public static function verify_ipn()
+	{
+		$raw_post_data = file_get_contents('php://input');
+
+		if (empty($raw_post_data))
+			return FALSE;
+
+		$raw_post_array = explode('&', $raw_post_data);
+		$post_data = array();
+
+		foreach ($raw_post_array as $keyval)
+		{
+			$keyval = explode('=', $keyval);
+			
+			if (count($keyval) === 2)
+			{
+				$post_data[$keyval[0]] = urldecode($keyval[1]);
+			}
+		}
+
+		$request_data = 'cmd=_notify-validate';
+
+		foreach ($post_data as $key => $value)
+		{
+			if (version_compare(PHP_VERSION, '5.4') < 0 AND get_magic_quotes_gpc())
+			{
+				$value = stripslashes($value);
+			}
+
+			$value = urlencode($value);
+
+			$request_data .= "&$key=$value";
+		}
+
+		$url = Payment::webscr_url();
+
+		$curl = curl_init($url);
+		curl_setopt_array($curl, array(
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_POST => 1,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_POSTFIELDS => $request_data,
+			CURLOPT_SSL_VERIFYPEER => 1,
+			CURLOPT_SSL_VERIFYHOST => 2,
+			CURLOPT_FORBID_REUSE => 1,
+			CURLOPT_HTTPHEADER => array(
+				'Connection: Close',
+			)
+		));
+
+		if ( ! ($response = curl_exec($curl)))
+		{
+			// Get the error code and message
+			$code  = curl_errno($curl);
+			$error = curl_error($curl);
+
+			// Close curl
+			curl_close($curl);
+
+			throw new Request_Exception('PayPal API request for :method failed: :error (:code)', $url, $post_data, array(
+				':method' => '_notify-validate',
+				':error' => $error,
+				':code' => $code
+			));
+		}
+
+		curl_close($curl);
+
+		if ($response === 'VERIFIED')
+			return TRUE;
+
+		if ($response === 'INVALID')
+			throw new Request_Exception('PayPal request to verify IPN was invalid!', $url, $post_data);
+
+		return FALSE;
+	}
+
 	protected $_config;
 
 	protected $_order = array();
@@ -196,82 +276,6 @@ abstract class Payment {
 		$this->_notify_url = $notify_url;
 
 		return $this;
-	}
-
-	/**
-	 * Validates an IPN request from Paypal.
-	 */
-	public function verify_ipn($data)
-	{
-		$raw_post_data = file_get_contents('php://input');
-		$raw_post_array = explode('&', $raw_post_data);
-		$post_data = array();
-
-		foreach ($raw_post_array as $keyval)
-		{
-			$keyval = explode('=', $keyval);
-			
-			if (count($keyval) === 2)
-			{
-				$post_data[$keyval[0]] = urldecode($keyval[1]);
-			}
-		}
-
-		$request_data = 'cmd=_notify-validate';
-
-		foreach ($post_data as $key => $value)
-		{
-			if (version_compare(PHP_VERSION, '5.4') < 0 AND get_magic_quotes_gpc())
-			{
-				$value = stripslashes($value);
-			}
-
-			$value = urlencode($value);
-
-			$request_data .= "&$key=$value";
-		}
-
-		$url = Payment::webscr_url();
-
-		$curl = curl_init($url);
-		curl_setopt_array($curl, array(
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_POST => 1,
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_POSTFIELDS => $request_data,
-			CURLOPT_SSL_VERIFYPEER => 1,
-			CURLOPT_SSL_VERIFYHOST => 2,
-			CURLOPT_FORBID_REUSE => 1,
-			CURLOPT_HTTPHEADER => array(
-				'Connection: Close',
-			)
-		));
-
-		if ( ! ($response = curl_exec($curl)))
-		{
-			// Get the error code and message
-			$code  = curl_errno($curl);
-			$error = curl_error($curl);
-
-			// Close curl
-			curl_close($curl);
-
-			throw new Request_Exception('PayPal API request for :method failed: :error (:code)', $url, $post_data, array(
-				':method' => '_notify-validate',
-				':error' => $error,
-				':code' => $code
-			));
-		}
-
-		curl_close($curl);
-
-		if ($response === 'VERIFIED')
-			return TRUE;
-
-		if ($response === 'INVALID')
-			throw new Request_Exception('PayPal request to verify IPN was invalid!', $url, $post_data);
-
-		return FALSE;
 	}
 
 	public function request($url, array $request_data = array(), array $headers = array())
