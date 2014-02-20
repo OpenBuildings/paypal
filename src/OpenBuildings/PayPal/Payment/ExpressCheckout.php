0cc0e67fc5264856e1277317fe1a2c5d6f6c6036
Michal Borychowski <boryn@o2.pl>
2014-02-20 11:20:54 +0100

    ExpressCheckout: order items are necessary to pass as request params

--- src/OpenBuildings/PayPal/Payment/ExpressCheckout.php
+++ src/OpenBuildings/PayPal/Payment/ExpressCheckout.php
@@ -26,53 +26,31 @@ class Payment_ExpressCheckout extends Payment {
 	 */
 	public function set_express_checkout(array $params = array())
 	{
+		$startParams = array(
+			'RETURNURL' => $this->return_url(),
+			'CANCELURL' => $this->cancel_url()
+		);
+		$params = array_merge($startParams, $params);
+
 		return $this->_request('SetExpressCheckout', $this->_set_params($params));
 	}
 
 	public function do_express_checkout_payment($token, $payer_id)
 	{
-		$order = $this->order();
+		$params = array(
+			'TOKEN'   => $token,
+			'PAYERID' => $payer_id
+		);
 
-		return $this->_request('DoExpressCheckoutPayment', array(
-			'TOKEN'                          => $token,
-			'PAYERID'                        => $payer_id,
-
-			// Total amount of the order
-			'PAYMENTREQUEST_0_AMT'           => number_format($order['total_price'], 2, '.', ''),
-
-			// Price of the items being sold
-			'PAYMENTREQUEST_0_ITEMAMT'       => number_format($order['items_price'], 2, '.', ''),
-
-			// Shipping costs for the whole transaction
-			'PAYMENTREQUEST_0_SHIPPINGAMT'   => number_format($order['shipping_price'], 2, '.', ''),
-
-			'PAYMENTREQUEST_0_CURRENCYCODE'  => $this->config('currency'),
-
-			'PAYMENTREQUEST_0_PAYMENTACTION' => 'Sale'
-		));
+		return $this->_request('DoExpressCheckoutPayment', $this->_set_params($params));
 	}
 
 	protected function _set_params(array $params = array())
 	{
-		$order = $this->order();
-
 		$defaultParams = array(
-			// Total amount for the transaction
-			'PAYMENTREQUEST_0_AMT' => number_format($order['total_price'], 2, '.', ''),
-
-			// Price of the items being sold
-			'PAYMENTREQUEST_0_ITEMAMT' => number_format($order['items_price'], 2, '.', ''),
-
-			// Shipping costs for the whole transaction
-			'PAYMENTREQUEST_0_SHIPPINGAMT' => number_format($order['shipping_price'], 2, '.', ''),
-
 			'PAYMENTREQUEST_0_CURRENCYCODE' => $this->config('currency'),
 
 			'PAYMENTREQUEST_0_PAYMENTACTION' => 'Sale',
-			
-			'RETURNURL' => $this->return_url(),
-
-			'CANCELURL' => $this->cancel_url(),
 
 			'useraction' => 'commit',
 
@@ -80,8 +58,22 @@ class Payment_ExpressCheckout extends Payment {
 			// For digital goods this field is required and it must be set to 1.
 			'NOSHIPPING' => 1,
 
+			'REQCONFIRMSHIPPING' => 0,
+
 			'ADDROVERRIDE' => 0,
 		);
+
+		$totalPrice = 0;
+		foreach($this->order() as $index => $item) {
+			$defaultParams['L_PAYMENTREQUEST_0_NAME' . $index] = $item["name"];
+			$defaultParams['L_PAYMENTREQUEST_0_AMT' . $index]  = $item["price"];
+			$defaultParams['L_PAYMENTREQUEST_0_QTY' . $index]  = $item["quantity"];
+			$defaultParams['L_PAYMENTREQUEST_0_ITEMCATEGORY' . $index] = "Digital";
+
+			$totalPrice += $item["price"] * $item["quantity"];
+		}
+		$params['PAYMENTREQUEST_0_AMT'] = number_format($totalPrice, 2, '.', '');
+
 		$params = array_merge($defaultParams, $params);
 
 		if ($this->notify_url())
